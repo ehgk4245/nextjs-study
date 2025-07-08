7f82b680056d4dbb11f592e4b3a1d15a5b25c41f
ehgk4245 <ehgkrk14@naver.com>
2025-07-08 12:48:17 +0900

    게시글 목록, 상세 API 연동

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
 function PostDetail() {
-  return <>상세</>;
+  const params = useParams();
+  const [post, setPost] = useState({});
+  useEffect(() => {
+    fetch(`https://dummyjson.com/posts/${params.id}`)
+      .then((res) => res.json())
+      .then((res) => setPost(res));
+  }, []);
+
+  return (
+    <>
+      <div>{post.id}</div>
+      <div>{post.title}</div>
+      <div>{post.body}</div>
+    </>
+  );
 }
 
 export default PostDetail;
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data.posts));
+  }, []);
   return (
     <>
       <ul>
-        <li>게시글1</li>
-        <li>게시글2</li>
-        <li>게시글3</li>
+        {posts.map((post) => (
+          <li key={post.id}>
+            {post.id} /{" "}
+            <Link
+              className="p-2 rounded hover:bg-red-100"
+              href={`/posts/${post.id}`}
+            >
+              {post.title}
+            </Link>
+          </li>
+        ))}
       </ul>
     </>
   );
